a038df53a43a0879353dc7e39afb72bc465e1fbf
Abdullah Arif <arifabdullah764@gmail.com>
2024-02-16 04:08:47 +0500

    new commit

---
+++ Software Eng/project/testing/first/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, C# World!");
+            string x = " ";
+            x = Console.ReadLine();
+            Console.WriteLine(x);
+            x = Console.ReadLine();
+
+
+        }
+    }
+}
